32075c3af2b9327b99d21cbfb055c6443eb8d3b0
hockie2 <hockie2@yahoo.com.sg>
2023-05-08 16:50:47 +0800

    update

--- src/index.js
+++ src/index.js
@@ -8,6 +8,30 @@ const cookieParser = require('cookie-parser');
  * ===================================
  */
 
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+
+
+
+
 // Init express app
 const app = express();
 
